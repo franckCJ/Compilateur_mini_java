03bb46924ab53e4af3f67e6f4689b5485bbcfd3d
Franck <franck@fcj.(none)>
2014-01-13 18:43:46 +0100

    Adding the writing of ast

--- Main/Compile.ml
+++ Main/Compile.ml
@@ -1,16 +1,11 @@
 let execute lexbuf verbose = 
 	try
-		Parsing_mjava.code Lexing_mjava.token lexbuf;
+		let ast = Parsing_mjava.code Lexing_mjava.token lexbuf in
+		Printf.printf ast
 	with
 		(* Gestion des erreurs personnalisées levées par le parser*)
 		| Ast.Compilation_Error (kind, pos) ->
 			Location.print pos;
 			Ast.report_error kind;
 			print_newline ();
-			Ast.File []
-		| Parsing_mjava.Error ->
-			Location.print (Location.curr lexbuf);
-			print_string "Syntax Error";
-			print_newline ();
-			Ast.File []
-			
+			(*Ast.File []*)
--- Parsing/ast.ml
+++ Parsing/ast.ml
@@ -1,5 +1,8 @@
 open Location
 
+
+(************ Partie Types AST *********)
+
 (*type opérateur binaire*)
 type binop =
 	| Badd | Bsub
@@ -38,16 +41,76 @@ type param =
 
 (*Eléments internes de la classe : méthodes ou attributs*)
 type class_intern =
-	(*methode : nom+type+static?+valeur*)
+	(*methode :     nom 	+  type  +static?+ valeur*)
 	| Attribute of string * string * bool * expression
-	(*methode : nom+type+static?+liste param+corps*)
+	(*methode :  nom   +  type  +static?+liste params+  corps*)
 	| Method of string * string * bool * param list * expression
 
-(*types de base de l'arbre*)
+(*Brique de bases d'un fichier, classes et expressions*)
 type minijava =
-	| File of minijava list
-	| Class of string * class_intern list
+	| Class of string * class_intern list * string
 	| Expression of expression
+
+(*type de base de l'arbre*)
+type ast = minijava list
+
+let rec display_list func elements =
+	match elements with
+		| []  	-> ""
+		| [e]  	-> func e
+		| t::q 	-> (func t) ^ "," ^ (display_list func q)
+
+let string_of_binop = function
+	| Badd 		-> "+"
+	| Bsub 		-> "-"
+	| Bmul 		-> "*"
+	| Bdiv 		-> "/"
+	| Bmod 		-> "%"
+	| Binf 		-> "<"
+	| Binfeq 	-> "<="
+	| Bsup 		-> ">"
+	| Bsupeq 	-> ">="
+	| Bequal 	-> "=="
+	| Bdiff 	-> "!="
+	| Band 		-> "&&"
+	| Bor 		-> "||"
+	| Bdel 		-> ";"
+
+let string_of_unop = function
+	| Uopposite -> "-"
+	| Unot 			-> "!"
+
+let rec string_of_expr = function
+	| None -> "None"
+	| Self -> "Self"
+	| Integer value -> string_of_int value
+	| String value -> value
+	| Boolean value -> string_of_bool value
+	| Variable name -> name
+	| Object name -> name
+	| Binop (op,expr1,expr2) 	-> "(" ^ (string_of_expr expr1) ^ (string_of_binop op) ^ (string_of_expr expr2) ^ ")"
+	| Unop (op,expr)  				-> (string_of_unop op) ^ (string_of_expr expr)
+	| Assignment (name,expr) 	-> name ^ "=" ^ (string_of_expr expr)
+	| Locassign (name,val_type,source,target) -> "(" ^ name ^","^ val_type ^ "," ^ (string_of_expr source) ^ ") -> " ^ (string_of_expr target)
+	| Condition (exprif,exprthen,exprelse) -> "Condition(" ^ (string_of_expr exprif) ^ "," ^ (string_of_expr exprthen) ^ "," ^ (string_of_expr exprelse) ^ ")"
+	| Method_call (caller,method_name,args) -> (string_of_expr caller) ^ "." ^ method_name ^ "(" ^ (display_list string_of_expr args) ^ ")"
+	| Cast (expr,val_type) -> "(" ^ val_type ^ ")" ^ (string_of_expr expr)
+
+let string_of_param = function
+	| Param (name,par_type) -> "(" ^ name ^ "," ^ par_type ^ ")"
+
+let string_of_func_interns = function
+	| Attribute (name,att_type,stat,expr) 		-> "Attribute (" ^ name ^ "," ^ att_type ^ "," ^ (string_of_bool stat) ^ "," ^ (string_of_expr expr) ^ ")"
+	| Method (name,ret_type,stat,params,expr)	-> "Method (" ^ name ^ "," ^ ret_type ^ "," ^ (string_of_bool stat) ^ "," ^ (display_list string_of_param params) ^ "," ^ (string_of_expr expr) ^ ")"
+
+let string_of_minijava element =
+	match element with
+		| Class (name,interns,mother) -> "Class (" ^ name ^ "," ^ "[" ^ (display_list string_of_func_interns interns) ^ "], " ^ mother ^ ")"
+		| Expression expr -> "Expr (" ^ (string_of_expr expr) ^ ")"
+
+let string_of_file file = "File ( " ^ (display_list string_of_minijava file) ^ " )"
+
+(************ Partie Erreurs *********)
 
 (*erreurs survenant sur les chaines de caractère*)
 type error_string =
